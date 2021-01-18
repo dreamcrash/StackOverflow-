a9885b61276ad51cc3a341631afad3b92b3357c0
Bruno Medeiros <lesiano16@gmail.com>
2021-01-18 23:00:11 +0100

    Adding padding with zeros, for the cases that the number of processes and the size are not perfectly divisable

--- MPI/SumInParallel/main.c
+++ MPI/SumInParallel/main.c
@@ -13,6 +13,7 @@ The problem is one of the processes receives two elements but the other one rece
 Here is what I have:
 
 **/
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
@@ -25,13 +26,17 @@ int main(int argc,char *argv[]){
     MPI_Comm_size(MPI_COMM_WORLD,&world_size);
 
     int size = (argc > 1) ? atoi(argv[1]) : 4;
+    int size_per_process = (size % world_size == 0) ? size / world_size : 1 + size / world_size;
+   
     int *number = NULL;
     if(world_rank == 0){
       number = malloc(sizeof(int) * size);
       for(int i = 0; i < size; i++)
          number[i] = i;
+      int size_with_padding = size_per_process * world_size;
+      for(int i = size; i < size_with_padding; i++)
+         number[i] = 0;
     }
-    int size_per_process = size / world_size;
     int local_number[size_per_process];
     MPI_Scatter(number, size_per_process, MPI_INT, local_number, size_per_process, MPI_INT, 0, MPI_COMM_WORLD);  
     int sub_sum = 0;
@@ -41,8 +46,9 @@ int main(int argc,char *argv[]){
     int sum = 0;
     MPI_Reduce(&sub_sum, &sum, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);            
     if(world_rank == 0){
-      int formula = (size * (size - 1)) / 2;
-      printf("\nthe sum of array is: %d == %d\n", sum, formula);
+      int sumNaturalNumbers = (size * (size - 1)) / 2;
+      assert(sum == sumNaturalNumbers);
+      printf("The sum of array is: %d == %d\n", sum, sumNaturalNumbers);
     }
 
     MPI_Finalize();
