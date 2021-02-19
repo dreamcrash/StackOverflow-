f4968ade5a70b93a0d1c8ca4c85a091dde7ce02a
Bruno Medeiros <lesiano16@gmail.com>
2021-02-19 22:45:51 +0100

    cleaning the code a bit

--- OpenMP/MatrixMultiplication/main.c
+++ OpenMP/MatrixMultiplication/main.c
@@ -16,25 +16,42 @@ Will the array C be accurate in both the versions regardless of the size of the 
 #include <stdio.h>
 #include <omp.h>
 
-void fillupRandomly (int *m, int dimension){
-    for (int i = 0; i < dimension; i++)
-        for (int j = 0; j < dimension; j++)
-            m[dimension*i+j] = (((float)rand())/RAND_MAX*99+1);
+unsigned int rand_interval(unsigned int min, unsigned int max)
+{
+    // https://stackoverflow.com/questions/2509679/
+    int r;
+    const unsigned int range = 1 + max - min;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+
+    do
+    {
+        r = rand();
+    } 
+    while (r >= limit);
+
+    return min + (r / buckets);
+}
+
+void fillupRandomly (int *m, unsigned int size, unsigned int min, unsigned int max){
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            m[size*i + j] = rand_interval(min, max);
 } 
 
-void matMultiply (int *A, int *B, int *C, int dimension) {
+void matMultiply (int *a, int *b, int *c, int size) {
     #pragma omp parallel for collapse(2)
-    for(int i = 0; i < dimension; i++)
-     for(int j = 0; j < dimension; j++) 
-       for(int k = 0; k < dimension; k++)
-           C[dimension*i+j] += A[dimension*i+k] *  B[dimension*k+j];        
+    for(int i = 0; i < size; i++)
+     for(int j = 0; j < size; j++) 
+       for(int k = 0; k < size; k++)
+           c[size*i+j] += a[size*i+k] *  b[size*k+j];        
   }
 
-void printMatrix(int *C, int N){
+void printMatrix(int *c, int size){
     printf("----- Matrix ------\n"); 
-    for(int i = 0; i < N; i++){
-       for(int j = 0; j < N; j++){
-          printf("%d ",C[N*i+j]);
+    for(int i = 0; i < size; i++){
+       for(int j = 0; j < size; j++){
+          printf("%d ",c[size*i+j]);
        }
        printf("\n");
     }
@@ -46,36 +63,39 @@ int main(int argc, char *argv[]) {
    	int N  = (argc > 1) ? atoi(argv[1]) : 10;
 	int print = (argc > 2) ? atoi(argv[2]) : 0;
         int numThreads = (argc > 3) ? atoi(argv[3]) : 2;
-	int *A = malloc(N * N * sizeof(int));
-	int *B = malloc(N * N * sizeof(int));
-	int *C = malloc(N * N * sizeof(int));
+	int *a = malloc(N * N * sizeof(int));
+	int *b = malloc(N * N * sizeof(int));
+	int *c = malloc(N * N * sizeof(int));
 
         omp_set_dynamic(0);              /** Explicitly disable dynamic teams **/
         omp_set_num_threads(numThreads); /** Use N threads for all parallel regions **/
 
          // Dealing with fail memory allocation
-    	if(!A || !B || !C)
+    	if(!a || !b || !c)
     	{
-        	if(A)   free(A);
-        	if(B)   free(B);
-        	if(C)   free(C);
-        	return (EXIT_FAILURE);
+          if(a)   free(a);
+          if(b)   free(b);
+          if(c)   free(c);
+          return (EXIT_FAILURE);
     	}
-	fillupRandomly (A, N);
-        fillupRandomly (B, N);
+	fillupRandomly (a, N, 0, 100);
+        fillupRandomly (b, N, 0, 100);
 
 	double begin = omp_get_wtime();
-	matMultiply(A, B, C, N);
+	matMultiply(a, b, c, N);
  	double end = omp_get_wtime();
    	printf("Time: %f\n",end-begin);
 
-	if(print){
-	  printMatrix(C, N);
+	if(print)
+        {
+	  printMatrix(a, N);
+	  printMatrix(b, N);
+	  printMatrix(c, N);
 	}
 
-	free(A);
-	free(B);
-	free(C);
+	free(a);
+	free(b);
+	free(c);
 	return (EXIT_SUCCESS);
 }
 
