67834fdcbe7e298c6a49f81e5fc75b1cc13c7865
Bruno Medeiros <lesiano16@gmail.com>
2021-03-07 16:58:18 +0100

    Script for logging admin session

---
+++ Keycloak/RestAdminAPI/logoutAdminSession.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KEYCLOAK_HOST=$1
+TOKEN="$2"
+
+if [[ $# -ne 2 ]]; then
+   	echo "Wrong number of parameters"
+   	echo "Usage: $0 <Keycloak Host> <Token>"
+        exit 1
+fi
+
+ACCESS_TOKEN=$(echo $TOKEN | jq -r .access_token)
+SESSION_STATE=$(echo $TOKEN | jq -r .session_state)
+
+curl -k 	-X DELETE http://$KEYCLOAK_HOST/auth/admin/realms/master/sessions/$SESSION_STATE \
+		-H "Content-Type: application/json" \
+		-H "Authorization: Bearer $ACCESS_TOKEN"
+ 
+
