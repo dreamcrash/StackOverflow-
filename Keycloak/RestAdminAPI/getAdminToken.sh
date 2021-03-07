1d39ee6584372a681a81481c8d383379382a4f94
Bruno Medeiros <lesiano16@gmail.com>
2021-03-07 16:43:23 +0100

    Script to get the admin token

---
+++ Keycloak/RestAdminAPI/getAdminToken.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+KEYCLOAK_HOST=$1
+ADMIN_NAME=$2
+ADMIN_PASSWORD=$3
+
+if [[ $# -ne 3 ]]; then
+        echo "Wrong number of parameters"
+    	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password>"
+        exit 1
+fi
+
+curl -k --request POST \
+  	--url http://$KEYCLOAK_HOST/auth/realms/master/protocol/openid-connect/token \
+  	--data client_id=admin-cli \
+  	--data username=$ADMIN_NAME \
+  	--data password=$ADMIN_PASSWORD \
+  	--data grant_type=password
