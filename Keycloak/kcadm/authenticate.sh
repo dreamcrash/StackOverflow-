dabedbb563b6ce3819de3477fe269bf5720cd8af
Bruno Medeiros <lesiano16@gmail.com>
2021-03-26 22:21:29 +0100

    improving scripts

--- Keycloak/kcadm/authenticate.sh
+++ Keycloak/kcadm/authenticate.sh
@@ -3,11 +3,15 @@ KEYCLOAK_IP=$1
 ADMIN_NAME=$2
 ADMIN_PASSWORD=$3
 
+set -e
+set -u -o pipefail
+SCRIPT_DIR="$(dirname "$0")"
+
 if [[ $# -ne 3 ]]; then
-	echo "Wrong number of parameters"
-	echo "Usage: $0 <keycloak_ip> <admin_name> <admin_password>"
+	echo "Wrong number of parameters" >&2
+	echo "Usage: $0 <keycloak_ip> <admin_name> <admin_password>" >&2
 	exit 1
 fi
 
 
-./kcadm.sh config credentials --server https://$KEYCLOAK_IP/auth --realm master --user $ADMIN_NAME --password $ADMIN_PASSWORD
+$SCRIPT_DIR/kcadm.sh config credentials --server https://$KEYCLOAK_IP/auth --realm master --user $ADMIN_NAME --password $ADMIN_PASSWORD
