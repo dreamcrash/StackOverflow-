24d0f554e06cdbd5278328ecedd511feb108ac15
Bruno Medeiros <lesiano16@gmail.com>
2021-04-03 12:03:25 +0200

    script to get the users

---
+++ Keycloak/RestAdminAPI/Users/getUsers.sh
@@ -0,0 +1,33 @@
+# Original Question: https://stackoverflow.com/questions/65200310
+ 
+#!/bin/bash
+KEYCLOAK_HOST="$1"
+ADMIN_NAME="$2"
+ADMIN_PASSWORD="$3"
+REALM_NAME="$4"
+USERNAME="$5"
+
+set -e 
+set -u -o pipefail
+
+SCRIPT_DIR="$(dirname "$0")"
+
+if [[ $# -ne 5 ]]; then
+        echo "Wrong number of parameters" >&2
+    	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password> <Realm Name> <Username>" >&2
+        exit 1
+fi
+
+ADMIN_TOKEN=$(sh $SCRIPT_DIR/../getAdminToken.sh "$KEYCLOAK_HOST" "$ADMIN_NAME" "$ADMIN_PASSWORD")
+ACCESS_TOKEN=$(echo $ADMIN_TOKEN | jq -r .access_token)
+
+
+USERNAME=$(curl -k -sS	-X GET http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/users/?username=$USERNAME \
+			-H "Content-Type: application/json" \
+			-H "Authorization: Bearer $ACCESS_TOKEN")
+
+echo $USERNAME | jq -r .[0]
+
+
+
+sh $SCRIPT_DIR/../logoutAdminSession.sh "$KEYCLOAK_HOST" "$ADMIN_TOKEN"
