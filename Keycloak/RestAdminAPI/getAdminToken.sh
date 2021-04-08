c320b308adfbcee6359d62eb93c4b0c8b07f314a
Bruno Medeiros <lesiano16@gmail.com>
2021-04-08 22:46:10 +0200

    abstract the directory way

--- Keycloak/RestAdminAPI/getAdminToken.sh
+++ Keycloak/RestAdminAPI/getAdminToken.sh
@@ -6,10 +6,12 @@ ADMIN_PASSWORD="$3"
 set -e
 set -u -o pipefail
 
+SCRIPT_DIR="$(dirname "$0")"
+
 if [[ $# -ne 3 ]]; then
         echo "Wrong number of parameters" >&2
     	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password>" >&2
         exit 1
 fi
 
-sh Token/getTokenGrantTypePassword.sh $KEYCLOAK_HOST "master" "admin-cli" $ADMIN_NAME $ADMIN_PASSWORD
+sh $SCRIPT_DIR/Token/getTokenGrantTypePassword.sh $KEYCLOAK_HOST "master" "admin-cli" $ADMIN_NAME $ADMIN_PASSWORD
