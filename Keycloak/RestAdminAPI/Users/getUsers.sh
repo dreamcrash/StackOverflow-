2b7d9a52bad019760aaaefd36cca3d8387503796
Bruno Medeiros <lesiano16@gmail.com>
2021-04-25 16:17:32 +0200

    Fix getUsers script

--- Keycloak/RestAdminAPI/Users/getUsers.sh
+++ Keycloak/RestAdminAPI/Users/getUsers.sh
@@ -1,33 +1,27 @@
 # Original Question: https://stackoverflow.com/questions/65200310
  
 #!/bin/bash
+set -e 
+set -u -o pipefail
+
+if [[ $# -ne 4 ]]; then
+        echo "Wrong number of parameters" >&2
+    	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password> <Realm Name>" >&2
+        exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$0")"
 KEYCLOAK_HOST="$1"
 ADMIN_NAME="$2"
 ADMIN_PASSWORD="$3"
 REALM_NAME="$4"
-USERNAME="$5"
-
-set -e 
-set -u -o pipefail
-
-SCRIPT_DIR="$(dirname "$0")"
-
-if [[ $# -ne 5 ]]; then
-        echo "Wrong number of parameters" >&2
-    	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password> <Realm Name> <Username>" >&2
-        exit 1
-fi
 
 ADMIN_TOKEN=$(sh $SCRIPT_DIR/../getAdminToken.sh "$KEYCLOAK_HOST" "$ADMIN_NAME" "$ADMIN_PASSWORD")
 ACCESS_TOKEN=$(echo $ADMIN_TOKEN | jq -r .access_token)
 
 
-USERNAME=$(curl -k -sS	-X GET "http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/users/?username=$USERNAME" \
+curl -k -sS	-X GET "http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/users" \
 			-H "Content-Type: application/json" \
-			-H "Authorization: Bearer $ACCESS_TOKEN")
-
-echo $USERNAME | jq -r .[0]
-
-
+			-H "Authorization: Bearer $ACCESS_TOKEN"
 
 sh $SCRIPT_DIR/../logoutAdminSession.sh "$KEYCLOAK_HOST" "$ADMIN_TOKEN"
