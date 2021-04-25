d1ec9ae51541e5e8afd4bcee8976e89813eba985
Bruno Medeiros <lesiano16@gmail.com>
2021-04-25 16:27:05 +0200

    script to get realm roles by name

---
+++ Keycloak/RestAdminAPI/Roles/getRealmRoleByName.sh
@@ -0,0 +1,30 @@
+# Original Question: https://stackoverflow.com/questions/65404692
+ 
+#!/bin/bash
+set -e 
+set -u -o pipefail
+
+if [[ $# -ne 5 ]]; then
+        echo "Wrong number of parameters" >&2
+    	echo "Usage: $0 <Keycloak Host> <Admin User Name> <Admin Password> <Realm Name> <Role Name>" >&2
+        exit 1
+fi
+
+SCRIPT_DIR="$(dirname "$0")"
+KEYCLOAK_HOST="$1"
+ADMIN_NAME="$2"
+ADMIN_PASSWORD="$3"
+REALM_NAME="$4"
+ROLE_NAME="$5"
+
+ADMIN_TOKEN=$(sh $SCRIPT_DIR/../getAdminToken.sh "$KEYCLOAK_HOST" "$ADMIN_NAME" "$ADMIN_PASSWORD")
+ACCESS_TOKEN=$(echo $ADMIN_TOKEN | jq -r .access_token)
+
+
+
+curl -k -sS	-X GET "http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/roles/$ROLE_NAME" \
+		-H "Content-Type: application/json" \
+		-H "Authorization: Bearer $ACCESS_TOKEN"
+
+
+sh $SCRIPT_DIR/../logoutAdminSession.sh "$KEYCLOAK_HOST" "$ADMIN_TOKEN"
