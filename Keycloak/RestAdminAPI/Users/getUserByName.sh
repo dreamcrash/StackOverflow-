c46cafd1cff2606e1b33be31a2f7e40d186296a3
Bruno Medeiros <lesiano16@gmail.com>
2021-05-24 14:43:34 +0200

    script will return the entire list of results

--- Keycloak/RestAdminAPI/Users/getUserByName.sh
+++ Keycloak/RestAdminAPI/Users/getUserByName.sh
@@ -21,12 +21,8 @@ ADMIN_TOKEN=$(sh $SCRIPT_DIR/../getAdminToken.sh "$KEYCLOAK_HOST" "$ADMIN_NAME" 
 ACCESS_TOKEN=$(echo $ADMIN_TOKEN | jq -r .access_token)
 
 
-USERNAME=$(curl -k -sS	-X GET "http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/users/?username=$USERNAME" \
+curl -k -sS -X GET "http://$KEYCLOAK_HOST/auth/admin/realms/$REALM_NAME/users/?username=$USERNAME" \
 			-H "Content-Type: application/json" \
-			-H "Authorization: Bearer $ACCESS_TOKEN")
-
-echo $USERNAME | jq -r .[0]
-
-
+			-H "Authorization: Bearer $ACCESS_TOKEN"
 
 sh $SCRIPT_DIR/../logoutAdminSession.sh "$KEYCLOAK_HOST" "$ADMIN_TOKEN"
