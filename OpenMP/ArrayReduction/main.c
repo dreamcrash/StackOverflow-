48da993e7c7a4d356ffad7e89aefe298910dbfaf
Bruno Medeiros <lesiano16@gmail.com>
2021-10-27 08:48:13 +0200

    add a more generic version, where the array of the size is explicitly set on the reduction clause

--- OpenMP/ArrayReduction/main.c
+++ OpenMP/ArrayReduction/main.c
@@ -2,22 +2,23 @@
 https://stackoverflow.com/questions/20413995
 **/
 
+#define S_SIZE 10
 #include <stdio.h>
 #include <time.h>
 #include <omp.h>
 int main ()
 {
   int A [] = {84, 30, 95, 94, 36, 73, 52, 23, 2, 13};
-  int S [10] = {0};
+  int S [S_SIZE] = {0};
 
-  #pragma omp parallel for reduction(+:S)
-  for (int n=0 ; n<10 ; ++n ){
+  #pragma omp parallel for reduction(+:S[:S_SIZE])
+  for (int n=0 ; n<S_SIZE ; ++n ){
     for (int m=0; m<=n; ++m){
       S[n] += A[m];
     }
   }
   int expected_output [] = {84, 114, 209, 303, 339, 412, 464, 487, 489, 502};   
-  for(int i = 0; i < 10; i++){
+  for(int i = 0; i < S_SIZE; i++){
       if(S[i] == expected_output[i])
         printf("%d\n", S[i]);
      else
